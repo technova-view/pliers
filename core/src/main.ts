75f9f8b7bd6c2b86991ab5f23caca64d2e474853
maruf <mdmarufbinsalim@gmail.com>
2026-02-15 07:42:05 +0600

    Feat: updated bootstrap log for nestjs server

--- core/src/main.ts
+++ core/src/main.ts
@@ -1,8 +1,3 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
@@ -14,7 +9,7 @@ async function bootstrap() {
   const port = process.env.PORT || 5000;
   await app.listen(port);
   Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+    `Pliers backend core is running on: http://localhost:${port}/${globalPrefix}`
   );
 }
 
