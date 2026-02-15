2c9d9ac9f90a98b2a8923eb751bb35dadde7d9a5
maruf <mdmarufbinsalim@gmail.com>
2026-02-15 08:04:26 +0600

    Feat: added swagger docs to the backend

--- core/src/main.ts
+++ core/src/main.ts
@@ -1,15 +1,23 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
+  const config = new DocumentBuilder()
+    .setTitle('Pliers: Core API')
+    .setDescription('API documentation for Core service')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/docs', app, document);
+
   const port = process.env.PORT || 5000;
   await app.listen(port);
   Logger.log(
-    `Pliers backend core is running on: http://localhost:${port}/${globalPrefix}`
+    `Pliers backend core is running on: http://localhost:${port}/api`
   );
 }
 
